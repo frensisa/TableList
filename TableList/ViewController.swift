96fdf194d8193361ee3a78321cbbd0805c9c1178
frensisa <112398338+frensisa@users.noreply.github.com>
2023-05-17 01:34:47 +0300

    Initial Commit

---
+++ TableList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableList
+//
+//  Created by frensisa daudze on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
